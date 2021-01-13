3bb1485616aade031aa8a734ebc64f574eb463ef
Martin Rothschink <martin.rothschink@axonet.de>
2021-01-13 16:27:08 +0100

    Projektdateien hinzufügen.

---
+++ CalendarConverter/RadForm1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace CalendarConverter
+{
+   partial class RadForm1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.radButton1 = new Telerik.WinControls.UI.RadButton();
+         this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+         this.listBox1 = new System.Windows.Forms.ListBox();
+         this.panel1 = new System.Windows.Forms.Panel();
+         this.radButton2 = new Telerik.WinControls.UI.RadButton();
+         this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+         ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+         this.panel1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // radButton1
+         // 
+         this.radButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.radButton1.Location = new System.Drawing.Point(12, 12);
+         this.radButton1.Name = "radButton1";
+         this.radButton1.Size = new System.Drawing.Size(110, 24);
+         this.radButton1.TabIndex = 0;
+         this.radButton1.Text = "Load Word docx";
+         this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+         // 
+         // openFileDialog1
+         // 
+         this.openFileDialog1.FileName = "openFileDialog1";
+         this.openFileDialog1.Filter = "Word|*.docx";
+         // 
+         // listBox1
+         // 
+         this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.listBox1.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.listBox1.FormattingEnabled = true;
+         this.listBox1.ItemHeight = 15;
+         this.listBox1.Location = new System.Drawing.Point(0, 54);
+         this.listBox1.Name = "listBox1";
+         this.listBox1.Size = new System.Drawing.Size(734, 516);
+         this.listBox1.TabIndex = 1;
+         // 
+         // panel1
+         // 
+         this.panel1.Controls.Add(this.radButton2);
+         this.panel1.Controls.Add(this.radButton1);
+         this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+         this.panel1.Location = new System.Drawing.Point(0, 0);
+         this.panel1.Name = "panel1";
+         this.panel1.Size = new System.Drawing.Size(734, 54);
+         this.panel1.TabIndex = 2;
+         // 
+         // radButton2
+         // 
+         this.radButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.radButton2.Location = new System.Drawing.Point(128, 12);
+         this.radButton2.Name = "radButton2";
+         this.radButton2.Size = new System.Drawing.Size(110, 24);
+         this.radButton2.TabIndex = 1;
+         this.radButton2.Text = "Save CSV";
+         this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+         // 
+         // RadForm1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(734, 570);
+         this.Controls.Add(this.listBox1);
+         this.Controls.Add(this.panel1);
+         this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.Name = "RadForm1";
+         // 
+         // 
+         // 
+         this.RootElement.ApplyShapeToControl = true;
+         this.Text = "Calendar Converter";
+         ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+         this.panel1.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private Telerik.WinControls.UI.RadButton radButton1;
+      private System.Windows.Forms.OpenFileDialog openFileDialog1;
+      private System.Windows.Forms.ListBox listBox1;
+      private System.Windows.Forms.Panel panel1;
+      private Telerik.WinControls.UI.RadButton radButton2;
+      private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+   }
+}
---
+++ CalendarConverter/RadForm1.cs
@@ -0,0 +1,195 @@
+﻿
+namespace CalendarConverter
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Drawing.Text;
+   using System.IO;
+   using System.Windows.Forms;
+   using Telerik.Windows.Documents.Flow.Model;
+
+   public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+   {
+      private const string Header = "VERANSTALTUNG,ORT,VERANSTALTER,STARTDATUM,STARTZEIT,ENDDATUM,ENDZEIT,GANZTÄGIG?,KATEGORIE,BESCHREIBUNG";
+      private const string Veranstalter = "GKG Kirchheim unter Teck";
+      private const string Category = "Gottesdienste";
+      private const int ShortDuration = 30;
+      private const int StdDuration = 60;
+
+      private RadFlowDocument m_Document;
+      private string m_Filename;
+      public RadForm1()
+      {
+         InitializeComponent();
+      }
+
+      private void radButton1_Click(object sender, EventArgs e)
+      {
+         if (openFileDialog1.ShowDialog() == DialogResult.OK)
+         {
+            OpenWord(openFileDialog1.FileName);
+            ConvertTables();
+         }
+      }
+
+      private void ConvertTables()
+      {
+         Log($"{Header}");
+         foreach (var section in m_Document.Sections)
+         {
+            foreach (var table in section.EnumerateChildrenOfType<Table>())
+            {
+               if (table.GridColumnsCount == 4)
+               {
+                  var date = GetDateAboveTable(table);
+                  TimeSpan startTime = TimeSpan.MinValue;
+                  foreach (var row in table.Rows)
+                  {
+                     bool abort = false;
+                     string[] cellText = new string[4];
+                     int i = 0;
+
+                     foreach (var cell in row.Cells)
+                     {
+                        var text = string.Empty;
+                        foreach (var run in cell.EnumerateChildrenOfType<Run>())
+                           text = text + run.Text;
+
+                        if (i == 0)
+                        {
+                           if (!text.ToUpper().Contains("UHR"))
+                              abort = true;
+                           else
+                           {
+                              text = text.Replace("Uhr", "");
+                              text = text.Replace(".", ":");
+                              startTime = TimeSpan.Parse(text);
+                           }
+                        }
+
+                        if (i != 1)
+                           cellText[i++] = text.Trim();
+                        else
+                           cellText[i++] = ExpandLocation(text.Trim());
+                     }
+
+                     if (abort)
+                        break;
+
+                     var duration = cellText[2].StartsWith("Rosen") ? ShortDuration : StdDuration;
+                     TimeSpan endTime = startTime.Add(TimeSpan.FromMinutes(duration));
+                     Log($"{cellText[2]},{cellText[1]},{Veranstalter},{date:d},{startTime:g},{date:d},{endTime:g},,{Category},{cellText[3]}");
+                  }
+               }
+            }
+         }
+      }
+
+      private DateTime GetDateAboveTable(Table table)
+      {
+         if (table.Parent is Section section)
+         {
+            for (int i = 0; i < section.Blocks.Count; ++i)
+            {
+               if (section.Blocks[i] == table)
+               {
+                  for (int j = i; j > 0; --j)
+                  {
+                     if (section.Blocks[j] is Paragraph p)
+                     {
+                        var text = string.Empty;
+                        foreach (var run in p.EnumerateChildrenOfType<Run>())
+                           text = text + run.Text;
+
+                        if (TextStartsWithDay(text.ToUpper()))
+                        {
+                           text = RemoveTrailingText(text);
+                           var date = DateTime.Parse(text);
+
+                           return date;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+
+         return DateTime.Now;
+      }
+
+      private string RemoveTrailingText(string text)
+      {
+         var pos = text.IndexOf("–", StringComparison.Ordinal);
+         if (pos > 0)
+            return text.Substring(0, pos - 1);
+         
+         pos = text.IndexOf("-", StringComparison.Ordinal);
+         if (pos > 0)
+            return text.Substring(0, pos - 1);
+
+         return text;
+      }
+
+      private bool TextStartsWithDay(string text)
+      {
+         return text.StartsWith("SONNTAG")
+                || text.StartsWith("MONTAG")
+                || text.StartsWith("DIENSTAG")
+                || text.StartsWith("MITTWOCH")
+                || text.StartsWith("DONNERSTAG")
+                || text.StartsWith("FREITAG")
+                || text.StartsWith("SAMSTAG");
+      }
+
+      private string ExpandLocation(string text)
+      {
+         if (text.StartsWith("SU,"))
+            return text.Replace("SU,", "St. Ulrich");
+         if (text.StartsWith("SM,"))
+            return text.Replace("SM,", "St. Markus");
+         if (text.StartsWith("PP,"))
+            return text.Replace("PP,", "Peter und Paul");
+         if (text.StartsWith("HK,"))
+            return text.Replace("HK,", "Heilig Kreuz");
+         if (text.StartsWith("MK,"))
+            return text.Replace("MK,", "Maria Königin");
+         if (text.StartsWith("SN,"))
+            return text.Replace("SN,", "St. Nikolaus");
+         if (text.StartsWith("SL,"))
+            return text.Replace("SL,", "St. Lukas");
+
+         return text;
+      }
+
+      private void Log(string log)
+      {
+         listBox1.SelectedIndex = listBox1.Items.Add(log);
+      }
+
+      private void OpenWord(string fileName)
+      {
+         Telerik.Windows.Documents.Flow.FormatProviders.Docx.DocxFormatProvider provider = new Telerik.Windows.Documents.Flow.FormatProviders.Docx.DocxFormatProvider(); 
+         using (Stream input = File.OpenRead(fileName)) 
+         { 
+            m_Document = provider.Import(input); 
+         }
+
+         m_Filename = fileName;
+         Text = "Calendar Converter  - " + fileName;
+      }
+
+      private void radButton2_Click(object sender, EventArgs e)
+      {
+         saveFileDialog1.FileName = Path.GetFileNameWithoutExtension(m_Filename) + ".csv";
+         if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+         {
+            var list = new List<string>();
+            foreach (var listBox1Item in listBox1.Items)
+            {
+               list.Add(listBox1Item.ToString());
+            }
+            File.WriteAllLines(saveFileDialog1.FileName, list);
+         }
+      }
+   }
+}
